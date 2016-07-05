df65019d839b114489989644901b023fee7bd44d
Cordavi <michaelamundsen@gmail.com>
2016-07-05 16:22:46 -0400

    Explored different properties and functions that can be called on string

---
+++ documentationPractice.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let name = "Audrey"
+let uppercaseName = name.uppercaseString
+print("\(uppercaseName)...\(uppercaseName)...\(uppercaseName)! WAKE UP!")
+name.stringByAppendingString(uppercaseName)
+name.lowercaseString
+name.endIndex
+name.startIndex
+name.characters.count
